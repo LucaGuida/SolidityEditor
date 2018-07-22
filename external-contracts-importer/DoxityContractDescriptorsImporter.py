215a1660d31e8799b9b2ec6dc839ed7ed34c8f44
Luca Guida <>
2018-07-22 12:38:59 +0200

    New DoxityContractDescriptorsImporter script to import contract descriptor files from Doxity

--- external-contracts-importer/DoxityContractDescriptorsImporter.py
+++ external-contracts-importer/DoxityContractDescriptorsImporter.py
@@ -8,17 +8,9 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 parentCwd = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 doxitySimpletonPath = os.path.abspath(os.path.join(parentCwd, os.pardir)) + '/SolidityExplorer/doxity-simpleton/'
 
-
-# Clean /DoxityJSONs folder from current path, if existing
-if os.path.exists(cwd + "/DoxityJSONs"):
-  shutil.rmtree(cwd + "/DoxityJSONs")
-os.makedirs(cwd + "/DoxityJSONs")
-
-
-fromDirectory = doxitySimpletonPath + "/doxity/pages/docs"
-toDirectory = cwd + "/DoxityJSONs"
+fromDirectory = doxitySimpletonPath + "/contract-descriptor-files"
+toDirectory = cwd + "/contract-descriptor-files"
 
 copy_tree(fromDirectory, toDirectory)
 
-print ("\nDoxityJSONs folder updated!\n")
-
+print ("\ncontract-descriptor-files folder updated with contract descriptors from Doxity!\n")
